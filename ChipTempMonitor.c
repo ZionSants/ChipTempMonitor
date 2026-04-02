d60b6de9ba59ae57c7cfcdfee21be52a698827c1
Zion Gabriel <115374703+ZionSants@users.noreply.github.com>
2026-04-02 14:08:20 -0300

    Configurações básicas do código

--- ChipTempMonitor.c
+++ ChipTempMonitor.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#define LEDV 13
+#define BTNA 5
+#define BTNB 6
 
+void configPinos() {
+    gpio_init(LEDV);
+    gpio_set_dir(LEDV, GPIO_OUT);
+    
+    gpio_init(BTNA);
+    gpio_set_dir(BTNA, GPIO_IN);
+    gpio_pull_up(BTNA);
+
+    
+    gpio_init(BTNB);
+    gpio_set_dir(BTNB, GPIO_IN);
+    gpio_pull_up(BTNB);
+}
 
 int main()
 {
     stdio_init_all();
+    configPinos();
 
     while (true) {
+
+        gpio_put(LEDV, 1);
         printf("Hello, world!\n");
         sleep_ms(100);
     }
