242e16722eba092a81969de707c71ab775fcb0c1
Zion Gabriel <115374703+ZionSants@users.noreply.github.com>
2026-04-02 18:29:11 -0300

    Sinal de alerta no display caso a temperatura passe de 35 graus

--- ChipTempMonitor.c
+++ ChipTempMonitor.c
@@ -7,17 +7,12 @@
 #define I2C_PORT i2c1
 #define SDA 14
 #define SCL 15
-#define LEDV 13 // Pino do led vermelho
 #define BTNA 5 // Pino do botão A
 #define BTNB 6 // Pino do botão B
 #define adcTemperatura 4 // Canal do sensor de temperatura interna
 
 // Função para configurar os pinos utilizados
-void configPinos() {
-    // Inicialização do led
-    gpio_init(LEDV);
-    gpio_set_dir(LEDV, GPIO_OUT);
-    
+void configPinos() { 
     // Inicialização dos botões
     gpio_init(BTNA);
     gpio_set_dir(BTNA, GPIO_IN);
@@ -31,6 +26,13 @@ void configPinos() {
     adc_init();
     adc_set_temp_sensor_enabled(true);
     adc_select_input(adcTemperatura);
+
+    // Inicialização do display
+    i2c_init(I2C_PORT, 400 * 1000);
+    gpio_set_function(SDA, GPIO_FUNC_I2C);
+    gpio_set_function(SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(SDA);
+    gpio_pull_up(SCL);
 }
 
 // Função para converter o valor do adc para graus Celsius
@@ -45,30 +47,28 @@ int main()
 {
     stdio_init_all();
     configPinos();
-
-    i2c_init(I2C_PORT, 400 * 1000);
-    gpio_set_function(SDA, GPIO_FUNC_I2C);
-    gpio_set_function(SCL, GPIO_FUNC_I2C);
-    gpio_pull_up(SDA);
-    gpio_pull_up(SCL);
-
     ssd1306_t display;
     ssd1306_init(&display, 128, 64, 0x3C, I2C_PORT);
-
 
     while (true) {
         uint16_t valoradc = adc_read();
 
         float temperatura = conversao(valoradc); // Chama a função conversão para temperatura em Celsius
 
+        if (temperatura < 35) {
         char texto[20];
         sprintf(texto, "Temperatura: %.1f C", temperatura);
-
         ssd1306_clear(&display);
         ssd1306_draw_string(&display, 0, 20, 1, texto);
         ssd1306_show(&display);
-
-        sleep_ms(1000);
+        } else {
+        char texto[20];
+        sprintf(texto, "Alerta! Temp > 35C");
+        ssd1306_clear(&display);
+        ssd1306_draw_string(&display, 0, 20, 1, texto);
+        ssd1306_show(&display);
+        }
+        sleep_ms(5000);
     }
     return 0;
 }
