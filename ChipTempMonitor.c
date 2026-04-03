cbc43a61f38352944777aa16904067e1be3844d2
Zion Gabriel <115374703+ZionSants@users.noreply.github.com>
2026-04-03 09:56:29 -0300

    Adição de comentários

--- ChipTempMonitor.c
+++ ChipTempMonitor.c
@@ -6,15 +6,19 @@
 #include "ssd1306.h"
 
 #define BUZZER 21 // Pino do buzzer
+
+// Display
 #define I2C_PORT i2c1
 #define SDA 14
 #define SCL 15
+
 #define BTNA 5 // Pino do botão A
 #define BTNB 6 // Pino do botão B
 #define adcTemperatura 4 // Canal do sensor de temperatura interna
 
-ssd1306_t display;
+ssd1306_t display; // Declaração global do display
 
+// Protótipos para função conseguir chamar a outra
 void buzzerAlerta(uint freq, uint duration);
 void analiseTemperatura(float temperatura);
 
@@ -47,39 +51,47 @@ void configPinos() {
     pwm_set_enabled(slice, false);
 }
 
+// Função que emite um som no buzzer por um tempo
 void buzzerAlerta(uint freq, uint duration) {
-    gpio_set_function(BUZZER, GPIO_FUNC_PWM);
+    gpio_set_function(BUZZER, GPIO_FUNC_PWM); // Religa o controle do pino
+
     uint slice = pwm_gpio_to_slice_num(BUZZER);
     uint channel = pwm_gpio_to_channel(BUZZER);
 
-    pwm_set_clkdiv(slice, 125.0f);
-    uint32_t wrap = (1000000 / freq) - 1;
+    pwm_set_clkdiv(slice, 125.0f); // Divisor de clock
 
+    //Determina a frequência do áudio
+    uint32_t wrap = (1000000 / freq) - 1;
     pwm_set_wrap(slice, (uint16_t)wrap);
+
     pwm_set_chan_level(slice, channel, wrap / 2); // 50% duty cycle
     pwm_set_enabled(slice, true);
     sleep_ms(duration);
     pwm_set_enabled(slice, false);
 
+    // Coloca o pino em low para evitar chiados
     gpio_set_function(BUZZER, GPIO_FUNC_SIO);
     gpio_set_dir(BUZZER, GPIO_OUT);
     gpio_put(BUZZER, 0);
 }
 
+// Análise da temperatura do sensor interno da placa
 void analiseTemperatura(float temperatura) {
     char texto[25];
-    ssd1306_clear(&display);
+    ssd1306_clear(&display); // Limpa o framebuffer para escrever nova informação
 
     if (temperatura < 35.0f) {
+        // Temperatura normal
         sprintf(texto, "Temperatura: %.1f C", temperatura);
         ssd1306_draw_string(&display, 0, 20, 1, texto);
     } else {
+        //Temperatura alta, emite mensagem diferente e som
         sprintf(texto, "Alerta! > 35C");
         ssd1306_clear(&display);
         ssd1306_draw_string(&display, 0, 20, 1, texto);
-        buzzerAlerta(523, 500);
+        buzzerAlerta(523, 500); // 523hz em 500ms
     }
-    ssd1306_show(&display);
+    ssd1306_show(&display); // Envia o framebuffer pro display
 }
 
 // Função para converter o valor do adc para graus Celsius
@@ -95,10 +107,12 @@ int main()
     stdio_init_all();
     configPinos();
 
+    // Inicialização do display via I2C
     ssd1306_init(&display, 128, 64, 0x3C, I2C_PORT);
     ssd1306_clear(&display);
     ssd1306_show(&display);
 
+    // Lê a temperatura e atualiza o display a cada 3 segundos
     while (true) {
         uint16_t valoradc = adc_read();
         float temperatura = conversao(valoradc); // Chama a função conversão para temperatura em Celsius
